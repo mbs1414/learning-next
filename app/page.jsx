b080384503e882ee918af2a5089d68782fd603a0
mohammad <mhdbabakhani14@gmail.com>
2025-11-01 22:53:32 +0330

    feat: add home component

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+    <div className="h-screen w-screen font-bold text-4xl text-white bg-violet-800 text-center">
+      Home
+    </div>
+    );
+};
+
+export default Home;
