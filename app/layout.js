bfd5ce1201a2110bfa2df4420ea8071ff48440d1
mohammad <mhdbabakhani14@gmail.com>
2025-11-02 23:43:12 +0330

    feat: set direction to rtl

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body dir="rtl">
         {children}
       </body>
     </html>
